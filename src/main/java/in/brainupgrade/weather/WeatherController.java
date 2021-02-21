155cd14d542c6bf02842d3ad857490fc124855ff
Rajesh G <rajesh@unigps.in>
2021-02-21 16:12:08 +0530

    added hostname

--- src/main/java/in/brainupgrade/weather/WeatherController.java
+++ src/main/java/in/brainupgrade/weather/WeatherController.java
@@ -1,5 +1,8 @@
 package in.brainupgrade.weather;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,83 +12,83 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WeatherController {
-    private final WeatherService weatherService;
-    private final WeatherHistoryService weatherHistoryService;
+	private final WeatherService weatherService;
+	private final WeatherHistoryService weatherHistoryService;
 
-    @Autowired
-    public WeatherController(WeatherServiceImpl weatherService, WeatherHistoryServiceImpl weatherHistoryService) {
-        this.weatherService = weatherService;
-        this.weatherHistoryService = weatherHistoryService;
-    }
+	@Autowired
+	public WeatherController(WeatherServiceImpl weatherService, WeatherHistoryServiceImpl weatherHistoryService) {
+		this.weatherService = weatherService;
+		this.weatherHistoryService = weatherHistoryService;
+	}
 
+	@GetMapping("/")
+	public String getWeatherHomeBase(Model model) {
+		return "redirect:/weather-home";
+	}
 
-    @GetMapping("/")
-    public String getWeatherHomeBase(Model model) {
-        model.addAttribute("followedCity", weatherHistoryService.getFollowedCity());
-        return "weatherHome";
-    }
+	@GetMapping("/weather-home")
+	public String getWeatherHome(Model model) {
+		model.addAttribute("followedCity", weatherHistoryService.getFollowedCity());
+		try {
+			model.addAttribute("hostname", InetAddress.getLocalHost().getHostName());
+			model.addAttribute("hostip", InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+		}
+		return "weatherHome";
+	}
 
-    @GetMapping("/weather-home")
-    public String getWeatherHome(Model model) {
-        model.addAttribute("followedCity", weatherHistoryService.getFollowedCity());
-        return "weatherHome";
-    }
+	@RequestMapping(value = "/set-weather", method = { RequestMethod.PUT, RequestMethod.POST })
+	public String setWeather(String city) {
+		if (city.trim().equals("")) {
+			return "weatherError";
+		}
+		boolean isWeatherSet = weatherService.setWeatherForecast(city);
+		if (isWeatherSet) {
+			return "redirect:/weather-view";
+		}
+		return "weatherError";
+	}
 
+	@RequestMapping(value = "/set-weather-history-city-home", method = { RequestMethod.PUT, RequestMethod.POST })
+	public String setWeatherHistoryCityHome(String city) {
+		boolean isCitySet = weatherHistoryService.setCityToFollow(city);
+		if (isCitySet) {
+			return "redirect:/weather-home";
+		}
+		return "weatherError";
+	}
 
-    @RequestMapping(value = "/set-weather", method = {RequestMethod.PUT, RequestMethod.POST})
-    public String setWeather(String city) {
-        if (city.trim().equals("")) {
-            return "weatherError";
-        }
-        boolean isWeatherSet = weatherService.setWeatherForecast(city);
-        if (isWeatherSet) {
-            return "redirect:/weather-view";
-        }
-        return "weatherError";
-    }
+	@GetMapping("/get-history")
+	public String getHistory(Model model) {
+		model.addAttribute("weatherHistory", weatherHistoryService.findByFollowedCity());
+		return "weatherHistory";
+	}
 
-    @RequestMapping(value = "/set-weather-history-city-home", method = {RequestMethod.PUT, RequestMethod.POST})
-    public String setWeatherHistoryCityHome(String city) {
-        boolean isCitySet = weatherHistoryService.setCityToFollow(city);
-        if (isCitySet) {
-            return "redirect:/weather-home";
-        }
-        return "weatherError";
-    }
+	@GetMapping("/get-whole-history")
+	public String getWholeHistory(Model model) {
+		model.addAttribute("weatherHistory", weatherHistoryService.findAll());
+		return "weatherHistory";
+	}
 
-    @GetMapping("/get-history")
-    public String getHistory(Model model) {
-        model.addAttribute("weatherHistory", weatherHistoryService.findByFollowedCity());
-        return "weatherHistory";
-    }
+	@GetMapping("/weather-view")
+	public String getWeather(Model model) {
+		WeatherInfo weatherInfo = weatherService.getWeatherInfo();
 
-    @GetMapping("/get-whole-history")
-    public String getWholeHistory(Model model) {
-        model.addAttribute("weatherHistory", weatherHistoryService.findAll());
-        return "weatherHistory";
-    }
+		model.addAttribute("city", weatherInfo.getCity());
+		model.addAttribute("temperature", weatherInfo.getTemperature());
+		model.addAttribute("weatherStateName", weatherInfo.getWeatherStateName());
+		model.addAttribute("iconLink", weatherInfo.getIconLink());
+		model.addAttribute("airPressure", weatherInfo.getAirPressure());
+		model.addAttribute("windSpeed", weatherInfo.getWindSpeed());
+		model.addAttribute("windDirectionCompass", weatherInfo.getWindDirectionCompass());
+		model.addAttribute("sunRise", weatherInfo.getSunRise());
+		model.addAttribute("sunSet", weatherInfo.getSunSet());
 
+		return "weatherView";
+	}
 
-    @GetMapping("/weather-view")
-    public String getWeather(Model model) {
-        WeatherInfo weatherInfo = weatherService.getWeatherInfo();
-
-        model.addAttribute("city", weatherInfo.getCity());
-        model.addAttribute("temperature", weatherInfo.getTemperature());
-        model.addAttribute("weatherStateName", weatherInfo.getWeatherStateName());
-        model.addAttribute("iconLink", weatherInfo.getIconLink());
-        model.addAttribute("airPressure", weatherInfo.getAirPressure());
-        model.addAttribute("windSpeed", weatherInfo.getWindSpeed());
-        model.addAttribute("windDirectionCompass", weatherInfo.getWindDirectionCompass());
-        model.addAttribute("sunRise", weatherInfo.getSunRise());
-        model.addAttribute("sunSet", weatherInfo.getSunSet());
-
-        return "weatherView";
-    }
-
-
-    @GetMapping("/go-to-home-page")
-    public String goToHomePage() {
-        return "redirect:/weather-home";
-    }
+	@GetMapping("/go-to-home-page")
+	public String goToHomePage() {
+		return "redirect:/weather-home";
+	}
 }
